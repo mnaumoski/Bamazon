8f30b6492da1bbc93022b0419cead0dee587d292
mnaumoski <naumoski.marija@gmail.com>
2016-10-23 23:31:25 -0400

    promt with two questions

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -5,13 +5,13 @@ var prompt = require('prompt');
 
 // define connection to already created database Bamazon in MySQL Workbench
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root", //Your username
-    password: "", //Your password
-    database: "Bamazon"
-})
-//when connection is established print the connectionThredID
+        host: "localhost",
+        port: 3306,
+        user: "root", //Your username
+        password: "", //Your password
+        database: "Bamazon"
+    })
+    //when connection is established print the connectionThredID
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
@@ -22,15 +22,42 @@ connection.connect(function(err) {
 
 // READ
 // create table
-connection.query('SELECT * FROM Products', function(err, res){
-  if(err) throw err;
-  console.log("=======================================================");
-  console.log("Products available for sale");
-  console.log("=======================================================");
-  for (var i = 0; i < res.length; i++) {
-    console.log("______________________________________________________");
-    console.log("|" +res[i].id + " | Product: " + res[i].ProductName+ " | Department: " + res[i].DepartmentName+ " | Price: $" +res[i].Price+ "|");
-    console.log("______________________________________________________");
-  }
-  console.log("========================================================");
+connection.query('SELECT * FROM Products', function(err, res) {
+    if (err) throw err;
+    console.log("=======================================================");
+    console.log("Products available for sale");
+    console.log("=======================================================");
+    for (var i = 0; i < res.length; i++) {
+        console.log("______________________________________________________");
+        console.log("|" + res[i].id + " | Product: " + res[i].ProductName + " | Department: " + res[i].DepartmentName + " | Price: $" + res[i].Price + "|");
+        console.log("______________________________________________________");
+    }
+    console.log("========================================================");
+    start();
 });
+
+var start = function() {
+    prompt.start();
+// The app then prompts users with **two messages**.
+// The first should ask them the ID of the product they would like to buy.
+// The second message should ask how many units of the product they would like to buy.
+
+  prompt.get([{
+      name: 'id',
+      required: true,
+      message: "What is the id of the product"
+    }, {
+      name: 'quantity',
+      required: true,
+      message: "How many items?"
+    }], function (err, result) {
+    // 
+    // Log the results. 
+    // 
+    console.log('Command-line input received:');
+    console.log('  id: ' + result.id);
+    console.log('  quantity: ' + result.quantity);
+  });
+}
+
+
--- README.md
+++ README.md
@@ -35,7 +35,7 @@ Challenge #1: Customer View (Minimum Requirement)
 
 Then create a Node application called **BamazonCustomer.js**. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
 
-The app should then prompt users with **two messages**.
+The app then prompts users with **two messages**.
 
 * The first should ask them the ID of the product they would like to buy.
 * The second message should ask how many units of the product they would like to buy.
