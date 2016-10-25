c4c458cdd80f0b88f67edcb1f000ded117454e29
mnaumoski <naumoski.marija@gmail.com>
2016-10-25 13:52:40 -0400

    opted for inquirer

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -2,6 +2,9 @@
 var mysql = require('mysql');
 // require prompt
 var prompt = require('prompt');
+// require inquirer
+var inquirer = require('inquirer');
+
 // define connection to already created database Bamazon in MySQL Workbench
 var connection = mysql.createConnection({
         host: "localhost",
@@ -29,34 +32,43 @@ connection.query('SELECT * FROM Products', function(err, res) {
     for (var i = 0; i < res.length; i++) {
         console.log("______________________________________________________");
         console.log("|" + res[i].id + " | Product: " + res[i].ProductName + " | Department: " + res[i].DepartmentName + " | Price: $" + res[i].Price + "|");
-        console.log("______________________________________________________");
+        // console.log("______________________________________________________");
     }
     console.log("========================================================");
     start();
 });
 
 var start = function() {
-    prompt.start();
-// The app then prompts users with **two messages**.
-// The first should ask them the ID of the product they would like to buy.
-// The second message should ask how many units of the product they would like to buy.
-console.log("Please enter the ID of the product you want and its quantity.")
-  prompt.get([{
-      message: "Enter a number",
-      name: 'id',
-      required: true,
-      pattern: /[0-9]/
+    // The app then prompts users with **two messages**.
+    // The first should ask them the ID of the product they would like to buy.
+    // The second message should ask how many units of the product they would like to buy.
+    console.log("Please enter the ID of the product you want and its quantity.")
+
+    inquirer.prompt([{
+        name: "id",
+        type: "input",
+        message: "What is the id number of the product you wish to purchase?",
+        validate: function(value) {
+            if (isNaN(value) == false) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }, {
-      name: 'quantity',
-      required: true,
-      message: "Enter a number",
-      pattern: /[0-9]/
-    }], function (err, result) {
-    // Log the results.
-    console.log('Command-line input received:');
-    console.log('Customer would like the product with id: ' + result.id);
-    console.log('Customer desires quantity of ' + result.quantity);
-  });
+        name: "q",
+        type: "input",
+        message: "How many units of the product would like to buy?",
+        validate: function(value) {
+            if (isNaN(value) == false) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }]).then(function(answer) {
+        console.log(answer.id);
+        console.log(answer);
+        console.log(answer.q);
+    })
 }
-
-
