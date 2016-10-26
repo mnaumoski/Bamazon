532b62b70e09798cf324a224047be66155263c41
mnaumoski <naumoski.marija@gmail.com>
2016-10-26 09:45:44 -0400

    Bamazon with updating inventory

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -20,46 +20,45 @@ connection.connect(function(err) {
 })
 
 // Running this application will first display all of the items available for sale.
-// Include the ids, names, and prices of products for sale.
 
-// READ
-// create table
+// READ from the table
 connection.query('SELECT * FROM Bamazon.Products', function(err, res) {
     if (err) throw err;
     console.log("=======================================================");
-    console.log("This is Bamazon. These products are available for sale");
+    console.log("This is Bamazon. See products available for sale");
     console.log("=======================================================");
     for (var i = 0; i < res.length; i++) {
-        console.log("______________________________________________________");
-        console.log("|" + res[i].id + " | Product: " + res[i].ProductName + " | Department: " + res[i].DepartmentName + " | Price: $" + res[i].Price + "|");
-        // console.log("______________________________________________________");
+        console.log("ID| " + res[i].id + " | Product: " + res[i].ProductName + " | Department: " + res[i].DepartmentName + " | Price: $" + res[i].Price + "|");
+        console.log("------------------------------------------------------");
+
     }
     console.log("========================================================");
+    console.log("");
+    // invoke start funciton
     start();
 });
 
 var start = function() {
-        // The app then prompts users with **two messages**.
-        // The first should ask them the ID of the product they would like to buy.
-        // The second message should ask how many units of the product they would like to buy.
+        // First prompt asks user for ID of the product they would like to buy.
         console.log("Please enter the ID of the product you want and its quantity.")
-
+        console.log("");
         inquirer.prompt([{
             name: "id",
             type: "input",
             message: "What is the id number of the product you wish to purchase?",
             validate: function(value) {
-                if (isNaN(value) == false) {
+                if (isNaN(value) == false && parseInt(value) > 0 && parseInt(value) <= 10) {
                     return true;
                 } else {
+                    console.log("")
+                    console.log("Please enter a number.")
                     return false;
                 }
             }
         }]).then(function(answer) {
 
-            var idOfProductToBuy = answer.id - 1;
-            console.log(idOfProductToBuy);
-
+            var idOfProductToBuy = answer.id - 1; //to be used as an index for the array
+// the second prompt asks the user about the quantity they need
             inquirer.prompt([{
                 name: "quantity",
                 type: "input",
@@ -69,45 +68,35 @@ var start = function() {
                         return true;
                     } else {
                         console.log("")
-                        console.log("You can only buy 10 items at this point. Please, try again.")
+                        console.log("Please enter a number.")
                         return false;
                     }
                 }
             }]).then(function(ans) {
-
-                // console.log(ans.quantity);
-                // console.log(idOfProductToBuy);
-
+                //var that caputres the quantity
                 var quantityToBuy = ans.quantity;
-                console.log("You would like to buy " + quantityToBuy + " items of the product with id of " + answer.id);
-
-
+                // print what user wants to do
+                console.log("You would like to buy " + quantityToBuy + " items of the product with id of " + answer.id + ".");
+                // select the column we need to use for updating the inventory
                 connection.query('SELECT StockQuantity FROM Bamazon.Products', function(err, res) {
-
                     if (err) throw err;
-                    console.log(res)
-                    console.log(res[idOfProductToBuy].StockQuantity)
+                    // print starting inventory 
+                    console.log("Starting inventory of the selected item is: " + res[idOfProductToBuy].StockQuantity)
+                    // quantity needed
                     console.log("Customer buys " + quantityToBuy);
+                    // variable that caputres the difference after purchase
                     var newQuantity = parseInt(res[idOfProductToBuy].StockQuantity - ans.quantity);
-                    console.log("Update ineventory " + newQuantity);
-
-
+                    // print new inventory
+                    console.log("Update ineventory: " + newQuantity + "." + " Progress seen in Bamazon Products table.");
+                    // update table in the back
                     connection.query("UPDATE products SET ? WHERE ?", [{
                         StockQuantity: newQuantity
                     }, {
                         id: answer.id
                     }], function(err, res) {
                         if (err) throw err;
-                        console.log(res)
-
-
-                    });
-                })
-
-
-            })
-        })
-    }
-    // UPDATE Products
-    // SET StockQuantity = newQuantity
-    // WHERE id=1
+                    });//close second query
+                }) //close first query
+            })//close second promise
+        }) //close first promise
+    }// close start();
